6e4a4f0a21fe6da587ec7b39fa0e25273d36cb7c
Clinton Wolfe <clinton@omniti.com>
2013-01-23 20:13:38 +0000

    metadata file, basic

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Clinton Wolfe"
+maintainer_email "clintoncwolfeNO.SPAM@gmail.com"
+license          "Modified BSD"
+description      "Use xmlstarlet to perform partial edits on XML files"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+name             "xml_edit"
