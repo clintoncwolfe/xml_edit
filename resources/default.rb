906316930a096dc2ceeec1862c5cc89c292cf849
Clinton Wolfe <clinton@omniti.com>
2013-01-23 20:08:59 +0000

    roughed in resource

---
+++ resources/default.rb
@@ -0,0 +1,9 @@
+actions :insert, :replace, :delete, :nothing
+
+attribute :path, :kind_of => String, :required => true
+attribute :cursor_xpath, :kind_of => String, :required => true
+attribute :payload, :kind_of => String, :required => false
+attribute :only_if_xpath, :kind_of => String, :required => false
+attribute :not_if_xpath, :kind_of => String, :required => false
+attribute :backups, :kind_of => Integer, :required => false, :default => 5
+
